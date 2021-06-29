d97a21578918ac05c64131f8a26f8dec147beea5
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-29 17:08:55 +0530

    Add files via upload

---
+++ StringValidate/IsNullOrEmpty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidate
+{
+    class IsNullOrEmpty: IStringValidate
+    {
+
+        bool StringValidator(String data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ StringValidate/StartsWith.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidate
+{
+    class StartsWith: IStringValidate
+    {
+
+        bool StringValidate(String data)
+        {
+            if (data.StartsWith("ex"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
