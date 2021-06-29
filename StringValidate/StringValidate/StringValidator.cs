6baed1a46bfa3bbec8de14182c068810086f3861
Ubed Patait <67995958+ubedullahpatait@users.noreply.github.com>
2021-06-29 14:32:54 +0530

    Add files via upload

---
+++ StringValidate/StringValidate/StringValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidate
+{
+   public class StringValidator
+    {
+        public bool IsValidString(String data)
+        {
+            if (string.IsNullOrEmpty(data))
+            {
+                return false;
+            }
+            if (data.Length > 8)
+            {
+                return false;
+            }
+            if (data.StartsWith("ex"))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+}
---
+++ StringValidate/StringValidate/UserData2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidate
+{
+   public class UserData2
+    {
+        public String name;
+
+
+        public String _Name(String name)
+        {
+            return name;
+        }
+
+
+
+    }
+}
